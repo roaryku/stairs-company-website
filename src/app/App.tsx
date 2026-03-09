fa86711b13db07fd873e86fb712be15b8a55cd55
Rodika Aryku <raryku@yahoo.com>
2026-03-09 15:59:33 -0700

    Router fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -20,7 +20,7 @@ function ScrollToTop() {
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <ScrollToTop />
       <div className="min-h-screen flex flex-col bg-white">
         <Navigation />
